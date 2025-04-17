53fbcc212cee7540a98d433b4ed34d8a368e48d7
nishantttt07 <nishant123chaurasia@gmail.com>
2025-04-17 15:36:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Home from "./pages/Home";
+import "./App.css";
+import Footer from "./components/Footer";
+
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Footer />
+    </>
+  );
+}
+
+
+export default App;
